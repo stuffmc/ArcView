de15a33607b961a5b7cfe5bcf0b5430c048234d8
StuFF mc <m+git@carrascomolina.com>
2020-12-17 18:44:55 +0100

    Initial Commit

---
+++ ArcView/ArcViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcViewApp.swift
+//  ArcView
+//
+//  Created by StuFF mc on 17.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ArcViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
